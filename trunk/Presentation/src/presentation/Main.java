744fe1921b07f891ecb25809a6f9f659a024d58d
massimini <massimini@fe8fc8ab-853c-0410-afc4-9ded63293db1>
2007-10-22 20:57:00 +0000

    Creation of the project

---
+++ trunk/Presentation/src/presentation/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Main.java
+ *
+ * Created on October 22, 2007, 2:53 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package presentation;
+
+/**
+ *
+ * @author Owner
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+        //Write all of your code here
+        //This is where Your code needs to go
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main main = new Main();
+    }
+    
+}
